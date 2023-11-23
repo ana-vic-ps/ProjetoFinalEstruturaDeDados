1ec435bb1b4e5ad6296f2bb39f9ec0baa1f48032
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-11-23 20:03:18 -0300

    adiciona logica principal de desenhar a tela e funcoes auxiliares
    Adiciona inicializador da cobra, função de remover nós do corpo e resetar posição necessárias para recomeçar, logica de desenhar a tela (borda, cobra e comida) no espaço apropriado

--- SnakeGame.c
+++ SnakeGame.c
@@ -55,6 +55,85 @@ void novaComida(Comida *comida) {
     comida->y = rand() % ALTURA;
 }
 
+void inicializar(Cobrinha *cobrinha, Comida *comida) {
+    cobrinha->cabeca = criarNo(LARGURA / 2, ALTURA / 2);
+    cobrinha->comprimento = 1;
+    cobrinha->direcao = 4; // Começa indo para a direita
+
+    // Posiciona a comida em algum lugar aleatório
+    srand(time(NULL));
+    novaComida(comida);
+}
+
+void removerNosExcetoCabeca(Cobrinha *cobrinha) {
+    if (cobrinha == NULL || cobrinha->cabeca == NULL) {
+        return; // Verificação para evitar operações inválidas em casos nulos ou listas vazias
+    }
+
+    ParteCobra *atual = cobrinha->cabeca->proximo;
+    while (atual != NULL) {
+        ParteCobra *proximo = atual->proximo;
+        free(atual);
+        atual = proximo;
+    }
+
+    // Deixe a cabeça como o único nó na lista
+    cobrinha->cabeca->proximo = NULL;
+    cobrinha->comprimento = 1;
+}
+
+void resetaPosicao(Cobrinha *cobrinha) {
+    if (cobrinha == NULL) {
+        return;
+    }
+    cobrinha->cabeca->x = LARGURA / 2;
+    cobrinha->cabeca->y = ALTURA / 2;
+    cobrinha->direcao = (rand() % 4) + 1;
+}
+
+void mostrarJogo(Cobrinha *cobrinha, Comida *comida) {
+    system("cls"); // Limpa a tela
+
+    // Desenha a área de jogo
+    for (int i = 0; i < LARGURA + 2; i++)
+        printf("-");
+    printf("\n");
+
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            if (j == 0)
+                printf("|"); // Parede esquerda
+
+            bool ehParteCorpo = false;
+            ParteCobra *atual = cobrinha->cabeca;
+            while (atual != NULL) {
+                if (atual->x == j && atual->y == i) {
+                    if (atual == cobrinha->cabeca) {
+                        printf("@");
+                    } else {
+                        printf("o");
+                    }
+                    // Corpo da cobrinha
+                    ehParteCorpo = true;
+                }
+                atual = atual->proximo;
+            }
+
+            if (j == LARGURA - 1 && !ehParteCorpo)
+                printf("|"); // Parede direita
+            else if (!ehParteCorpo && i == comida->y && j == comida->x)
+                printf("F"); // Comida
+            else if (!ehParteCorpo)
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < LARGURA + 2; i++)
+        printf("#");
+    printf("\n");
+}
+
 int main(){
 
 }
