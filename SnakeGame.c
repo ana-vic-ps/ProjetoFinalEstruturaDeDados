7f5a364ebcb796bc2b3c9771fff34c39dad66877
Gabriel Pereira <30956246+oPatao@users.noreply.github.com>
2023-11-23 21:33:25 -0300

    Update SnakeGame.c
    add: recomeça o jogo do inicio, capturar entrada, movimento, verificaçao de colisão.

--- SnakeGame.c
+++ SnakeGame.c
@@ -134,6 +134,98 @@ void mostrarJogo(Cobrinha *cobrinha, Comida *comida) {
     printf("\n");
 }
 
+void recomecaJogoAtual(Cobrinha *cobrinha, Comida *comida)
+{
+    if (pontuacao > maiorPontuacao) 
+    {
+        maiorPontuacao = pontuacao;
+    }
+    pontuacao = 0;
+    removerNosExcetoCabeca(cobrinha);
+    resetaPosicao(cobrinha);
+    novaComida(comida);
+}
+
+void capturarEntrada(Cobrinha *cobrinha, Comida *comida) 
+{
+    if (_kbhit()) 
+    {
+        switch (_getch()) 
+        {
+            case 'w':
+                if (cobrinha->direcao != 2)
+                    cobrinha->direcao = 1;
+                break;
+            case 's':
+                if (cobrinha->direcao != 1)
+                    cobrinha->direcao = 2;
+                break;
+            case 'a':
+                if (cobrinha->direcao != 4)
+                    cobrinha->direcao = 3;
+                break;
+            case 'd':
+                if (cobrinha->direcao != 3)
+                    cobrinha->direcao = 4;
+                break;
+            case 'r':
+                recomecaJogoAtual(cobrinha, comida);
+                break;
+        }
+    }
+}
+
+void mover(Cobrinha *cobrinha) 
+{
+    int prevX = cobrinha->cabeca->x;
+    int prevY = cobrinha->cabeca->y;
+    ParteCobra *atual = cobrinha->cabeca->proximo;
+
+    int tempX, tempY;
+    while (atual != NULL) 
+    {
+        tempX = atual->x;
+        tempY = atual->y;
+        atual->x = prevX;
+        atual->y = prevY;
+        prevX = tempX;
+        prevY = tempY;
+        atual = atual->proximo;
+    }
+
+    switch (cobrinha->direcao) 
+    {
+        case 1:
+            cobrinha->cabeca->y--;
+            break;
+        case 2:
+            cobrinha->cabeca->y++;
+            break;
+        case 3:
+            cobrinha->cabeca->x--;
+            break;
+        case 4:
+            cobrinha->cabeca->x++;
+            break;
+    }
+}
+
+bool verificarColisao(Cobrinha *cobrinha) 
+{
+    if (cobrinha->cabeca->x < 0 || cobrinha->cabeca->x >= LARGURA || cobrinha->cabeca->y < 0 || cobrinha->cabeca->y >= ALTURA)
+        return true;
+
+    ParteCobra *atual = cobrinha->cabeca->proximo;
+    while (atual != NULL) 
+    {
+        if (atual->x == cobrinha->cabeca->x && atual->y == cobrinha->cabeca->y)
+            return true;
+        atual = atual->proximo;
+    }
+
+    return false;
+}
+
 bool verificarColisaoComida(Cobrinha *cobrinha, Comida *comida) {
     if (cobrinha->cabeca->x == comida->x && cobrinha->cabeca->y == comida->y) {
         pontuacao += 1;
@@ -194,4 +286,5 @@ void mostraInformacoes() {
 
 int main(){
 
+    return 0;
 }
